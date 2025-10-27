22dfe379f9f7194e00aac6abfa984ce26f9b5785
MohammadBnei <mohammadamine.banaei@pm.me>
2025-10-27 19:42:42 +0100

    working

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '../generated/prisma/client.js';
 import { DATABASE_URL } from '$env/dynamic/private';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient({
--- src/lib/server/db/schema.ts
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed as its content has been migrated to prisma/schema.prisma
-// You can safely delete this file.
