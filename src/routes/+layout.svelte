a53aabbc145bceef845172d94d0169923ebb63bb
MohammadBnei <mohammadamine.banaei@pm.me>
2025-10-30 13:42:26 +0100

    fix: Update favicon to .ico format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import * as m from '$lib/paraglide/messages';
 	import type { LayoutProps } from './$types';
 	import { onMount } from 'svelte';
