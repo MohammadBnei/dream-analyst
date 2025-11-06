9c65dc48fb7e35f4f1341cef0f5b3be372aa783c
MohammadBnei <mohammadamine.banaei@pm.me>
2025-11-06 10:49:33 +0100

    way to go

--- src/lib/chatService.ts
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed as its functionality has been moved to src/lib/server/services/chatService.ts
-// and src/lib/client/services/chatService.ts.
-// It can be safely deleted.
