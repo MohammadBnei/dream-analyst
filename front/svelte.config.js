25b758dcf472b5da6bc9b3255f709f4d741266aa
MohammadBnei <mohammadamine.banaei@pm.me>
2026-02-03 12:46:02 +0100

    fix

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -20,14 +20,6 @@ const config = {
 		},
 		csrf: {
 			trustedOrigins: ['*']
-		},
-		hooks: {
-			getSession({ request }) {
-        const origin = request.headers.get('origin') || 
-                      request.headers.get('x-forwarded-host') || 
-                      request.headers.get('host');
-        return { origin };
-      }
 		}
 	},
 	extensions: ['.svelte'],
